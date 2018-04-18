eff2d6951bdf330280ee8d2b62d9a12adb2cdc16
Jeff Zohrab <jzohrab@gmail.com>
2018-04-18 12:28:49 -0400

    Fix Rakefile generation script.

--- create_stub_klick_genome_rakefile.rb
+++ create_stub_klick_genome_rakefile.rb
@@ -5,16 +5,16 @@
 # - the Rakefile is nicely formatted (tasks in namespaces are indented)
 
 src = File.expand_path(File.join(File.dirname(__FILE__), '..', 'klick-genome', 'Rakefile'))
-puts src
+puts "Generating Rakefile from #{src}"
 raise "Missing #{src}" if (!File.exist?(src))
 
 r = File.read(src)
 skeleton =
   r.
     split("\n").
-    select { |lin| lin =~ /^\s*namespace/ || lin =~ /^\s*task/ }
-puts skeleton
-puts "===="
+    select { |lin| lin =~ /^\s*namespace/ || lin =~ /^\s*task/ || lin =~ /^\s*desc/ }
+# puts skeleton
+# puts "===="
 
 add_namespace_end_keywords = []
 
@@ -29,4 +29,31 @@ skeleton.each_with_index do | lin, index |
   end
 end
 
-puts add_namespace_end_keywords
+# puts add_namespace_end_keywords
+# puts "==="
+
+body = add_namespace_end_keywords.map do |lin|
+  if lin =~ /^\s*task/
+    taskname = lin.match(/task\s+:(.*?)\s/)[1]
+    "#{lin} puts \"  called #{taskname}\" end"
+  else
+    lin
+  end
+end
+
+# puts "==="
+# puts "BODY"
+# puts body
+
+File.open('Rakefile', 'w') do |f|
+  f.puts "### GENERATED FILE ###"
+  f.puts "# This file was generated with the script"
+  f.puts "# #{__FILE__}"
+  f.puts ''
+end
+
+File.open('Rakefile', 'a') do |f|
+  f.puts body.join("\n")
+end
+
+puts "Done."
