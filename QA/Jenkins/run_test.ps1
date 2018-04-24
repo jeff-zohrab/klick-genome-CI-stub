fdfdf22a2ca73cda434c72663b0f347e2a333668
Jeff Zohrab <jzohrab@gmail.com>
2018-04-24 08:44:12 -0400

    wip

--- QA/Jenkins/run_test.ps1
+++ QA/Jenkins/run_test.ps1
@@ -15,8 +15,9 @@ echo ">>>>>> FAKE RUN OF SELENIUM"
 echo "run_test.ps1 called with args:"
 echo " ${selargs}"
 
-Add-Content -Path "c:\www\report_runs.txt" -Value Get-Date
-Add-Content -Path "c:\www\report_runs.txt" -Value "`r`n${selargs}"
+$dt = Get-Date
+Add-Content -Path "c:\www\report_runs.txt" -Value $dt
+Add-Content -Path "c:\www\report_runs.txt" -Value "${selargs}"
 
 $path = "$PSScriptRoot\simulate_selenium_failure.txt"
 if (Test-Path $path) {
