72930bac0f48e3206ab6a4b4af4eaf0b02112bd7
Jeff Zohrab <jzohrab@gmail.com>
2018-04-24 15:07:17 -0400

    WIP

--- simulate_failure/simulate_failure_for_current_branch.rb
+++ simulate_failure/simulate_failure_for_current_branch.rb
@@ -12,14 +12,14 @@ end
 $selenium_failure_flag_file = File.expand_path(File.join(File.dirname(__FILE__), '..', 'QA', 'Jenkins', 'simulate_selenium_failure.txt'))
 
 def set_selenium_success()
-  puts "CLEARING FAILURE file at #{$selenium_failure_flag_file}"
+  puts "clearing failure file at #{$selenium_failure_flag_file}"
   if (File.exist?($selenium_failure_flag_file)) then
     File.delete($selenium_failure_flag_file)
   end
 end
 
 def set_selenium_failure()
-  puts "SETTING FAILURE file at #{$selenium_failure_flag_file}"
+  puts "SETTING failure file at #{$selenium_failure_flag_file}"
   File.open($selenium_failure_flag_file, 'w') do |f|
     f.puts "FAIL"
   end
@@ -37,10 +37,11 @@ end
 set_selenium_success()
 
 branch_name = get_git_branch_name()
-puts "Got branch: '#{branch_name}'"
+simulate_errors = File.join(File.dirname(__FILE__), "#{branch_name.gsub('/', '_')}.rb")
 
-simulate_errors = File.join(File.dirname(__FILE__), "#{branch_name}.rb")
+puts "Got branch: '#{branch_name}', checking for err simulation at #{simulate_errors}"
 if (File.exist?(simulate_errors)) then
+  puts "Simulating errors ..."
   require simulate_errors
 end
 
