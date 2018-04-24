ece70ffc93a60bf959f0253c5b6d562a0cc7e86c
Jeff Zohrab <jzohrab@gmail.com>
2018-04-24 15:29:14 -0400

    wip

--- simulate_failure/release_1234.rb
+++ simulate_failure/release_1234.rb
@@ -1,6 +1,6 @@
-Rake.application.remove_task(:runtests)
-task :runtests, :filter do |t, args|
-  raise 'simulating :runtests failure'
-end
+# Rake.application.remove_task(:runtests)
+# task :runtests, :filter do |t, args|
+#   raise 'simulating :runtests failure'
+# end
 
 set_selenium_failure()
