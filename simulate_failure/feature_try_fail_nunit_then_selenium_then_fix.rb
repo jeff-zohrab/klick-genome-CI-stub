ece1ab47a7166c60948088eb3b715d016cbaa600
Jeff Zohrab <jzohrab@gmail.com>
2018-05-22 16:28:44 -0400

    Update branch config

--- simulate_failure/feature_try_fail_nunit_then_selenium_then_fix.rb
+++ simulate_failure/feature_try_fail_nunit_then_selenium_then_fix.rb
@@ -1,6 +1,6 @@
 # Rake.application.remove_task(:runtests)
-# task :runtests, :filter do |t, args|
-#   raise 'simulating :runtests failure'
-# end
+task :runtests, :filter do |t, args|
+  raise 'simulating :runtests failure'
+end
 
-# set_selenium_failure()
+set_selenium_failure()
